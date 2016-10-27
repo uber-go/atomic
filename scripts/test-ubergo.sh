bf1fa78e294e0a140824f17c80204b6c6c41afd7
Prashant Varanasi <prashant@uber.com>
2016-10-27 00:25:10 -0700

    Remove debugging from script

--- scripts/test-ubergo.sh
+++ scripts/test-ubergo.sh
@@ -6,23 +6,16 @@ IFS=$'\n\t'
 # directory as uber-go/atomic and verifies that tests still pass.
 
 WORK_DIR=`mktemp -d`
-
-echo "work dir $WORK_DIR"
 function cleanup {
-	# rm -rf "$WORK_DIR"
-	echo "WORK DIR"
+	rm -rf "$WORK_DIR"
 }
 trap cleanup EXIT
 
 
 export GOPATH="$WORK_DIR"
-
 PKG_PARENT="$WORK_DIR/src/github.com/uber-go"
 PKG_DIR="$PKG_PARENT/atomic"
 
-echo "pkg parent $PKG_PARENT"
-echo "pkg dir $PKG_DIR"
-echo ln -s `pwd` "PKG_DIR"
 mkdir -p "$PKG_PARENT"
 ln -s `pwd` "$PKG_DIR"
 cd "$PKG_DIR"
