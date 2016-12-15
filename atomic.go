3b8db5e93c4c02efbc313e17b2e796b0914a01fb
Anton Tyurin <noxiouz@yandex.ru>
2016-12-15 20:56:52 +0100

    Implement Add/Sub for Float64 (#17)
    Signed-off-by: Anton Tiurin <noxiouz@yandex.ru>

--- atomic.go
+++ atomic.go
@@ -278,6 +278,22 @@ func (f *Float64) Store(s float64) {
 	atomic.StoreUint64(&f.v, math.Float64bits(s))
 }
 
+// Add atomically adds to the wrapped float64 and returns the new value.
+func (f *Float64) Add(s float64) float64 {
+	for {
+		old := f.Load()
+		new := old + s
+		if f.CAS(old, new) {
+			return new
+		}
+	}
+}
+
+// Sub atomically subtracts from the wrapped float64 and returns the new value.
+func (f *Float64) Sub(s float64) float64 {
+	return f.Add(-s)
+}
+
 // CAS is an atomic compare-and-swap.
 func (f *Float64) CAS(old, new float64) bool {
 	return atomic.CompareAndSwapUint64(&f.v, math.Float64bits(old), math.Float64bits(new))
