a59165dd2f3d6b5b195a8196e7338d5a55864a1e
Abhinav Gupta <abg@uber.com>
2020-05-12 13:52:49 -0700

    test/comparability: Test all types

--- nocmp_test.go
+++ nocmp_test.go
@@ -52,6 +52,18 @@ func TestNocmpComparability(t *testing.T) {
 			give:       &struct{ nocmp }{},
 			comparable: true,
 		},
+
+		// All exported types must be uncomparable.
+		{desc: "Bool", give: Bool{}},
+		{desc: "Duration", give: Duration{}},
+		{desc: "Error", give: Error{}},
+		{desc: "Float64", give: Float64{}},
+		{desc: "Int32", give: Int32{}},
+		{desc: "Int64", give: Int64{}},
+		{desc: "String", give: String{}},
+		{desc: "Uint32", give: Uint32{}},
+		{desc: "Uint64", give: Uint64{}},
+		{desc: "Value", give: Value{}},
 	}
 
 	for _, tt := range tests {
