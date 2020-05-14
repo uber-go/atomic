98666305b5011171baf5eb530e6551aa683d5d6b
Abhinav Gupta <abg@uber.com>
2020-05-14 15:27:16 -0700

    Generalize gen-valuewrapper into atomicwrapper
    valuewrapper can be used only to generate atomic wrapper types for
    types which are stored as atomic.Value. This isn't necessary because the
    same logic can be applied to atomic wrappers built on other types like
    Duration and Bool.
    
    Generalize valuewrapper into atomicwrapper with support for optional
    pack/unpack functions which handle conversion to/from `interface{}`.
    This lets Error hook in and install the `storedError` struct (now called
    `packedError`) to avoid panics from nil storage or value type change.

--- error_ext.go
+++ error_ext.go
@@ -20,24 +20,20 @@
 
 package atomic
 
-//go:generate bin/gen-atomicwrapper -name=String -type=string -wrapped=Value -file=string.go
+// atomic.Value panics on nil inputs, or if the underlying type changes.
+// Stabilize by always storing a custom struct that we control.
 
-// String returns the wrapped value.
-func (s *String) String() string {
-	return s.Load()
+//go:generate bin/gen-atomicwrapper -name=Error -type=error -wrapped=Value -pack=packError -unpack=unpackError -file=error.go
+
+type packedError struct{ Value error }
+
+func packError(v error) interface{} {
+	return packedError{v}
 }
 
-// MarshalText encodes the wrapped string into a textual form.
-//
-// This makes it encodable as JSON, YAML, XML, and more.
-func (s *String) MarshalText() ([]byte, error) {
-	return []byte(s.Load()), nil
-}
-
-// UnmarshalText decodes text and replaces the wrapped string with it.
-//
-// This makes it decodable from JSON, YAML, XML, and more.
-func (s *String) UnmarshalText(b []byte) error {
-	s.Store(string(b))
+func unpackError(v interface{}) error {
+	if err, ok := v.(packedError); ok {
+		return err.Value
+	}
 	return nil
 }
