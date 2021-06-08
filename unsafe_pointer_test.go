117764f170f2680f123e94ebbc03f89c94be3e72
tison <wander4096@gmail.com>
2021-06-08 23:11:08 +0800

    unsafe_pointer: introduce
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ unsafe_pointer_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package atomic
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"unsafe"
+)
+
+func TestUnsafePointer(t *testing.T) {
+	i := int64(42)
+	j := int64(0)
+	k := int64(1)
+	atom := NewUnsafePointer(unsafe.Pointer(&i))
+
+	require.Equal(t, unsafe.Pointer(&i), atom.Load(), "Load didn't work.")
+	require.True(t, atom.CAS(unsafe.Pointer(&i), unsafe.Pointer(&j)), "CAS didn't report a swap.")
+	require.Equal(t, unsafe.Pointer(&j), atom.Load(), "CAS didn't set the correct value.")
+
+	require.Equal(t, unsafe.Pointer(&j), atom.Swap(unsafe.Pointer(&k)), "Swap didn't return the old value.")
+	require.Equal(t, unsafe.Pointer(&k), atom.Load(), "Swap didn't set the correct value.")
+
+	atom.Store(unsafe.Pointer(&i))
+	require.Equal(t, unsafe.Pointer(&i), atom.Load(), "Store didn't set the correct value.")
+
+	require.Equal(t, uint64(42), *(*uint64)(atom.Load()), "UnsafePointer failed to convert to another type.")
+}
